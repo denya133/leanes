2f6ba88ee37fabc20025c5b3d94d0245ea41fbe3
iramarkjs <ira@jspro.dev>
2020-07-06 11:48:12 +0300

    add container in Controller

--- src/leanes/patternes/core/Controller.js
+++ src/leanes/patternes/core/Controller.js
@@ -2,7 +2,7 @@ import type { ControllerInterface } from '../interfaces/ControllerInterface';
 import type { ViewInterface } from '../interfaces/ViewInterface';
 import type { CommandInterface } from '../interfaces/CommandInterface';
 import type { NotificationInterface } from '../interfaces/NotificationInterface';
-
+import { injectable, inject, Container } from "inversify";
 
 export default (Module) => {
   const {
@@ -13,7 +13,9 @@ export default (Module) => {
     Utils: { _ }
   } = Module.NS;
 
+  let container = new Container();
 
+  @injectable
   @initialize
   @module(Module)
   class Controller extends CoreObject implements ControllerInterface {
@@ -25,7 +27,7 @@ export default (Module) => {
     // ipoView         = PointerT @private _view: ViewInterface
     @property _view: ViewInterface = null;
     // iphCommandMap   = PointerT @private _commandMap: DictG String, MaybeG SubsetG CommandInterface
-    @property _commandMap: {[key: string]: ?Class<CoreObject>} = null;
+    // @property _commandMap: {[key: string]: ?Class<CoreObject>} = null;
     // iphClassNames   = PointerT @private _classNames: DictG String, MaybeG String
     @property _classNames: {[key: string]: ?string} = null;
     // ipsMultitonKey  = PointerT @protected _multitonKey: MaybeG String
@@ -76,8 +78,11 @@ export default (Module) => {
     @method static removeController(asKey: string): void {
       const voController = Controller._instanceMap[asKey]
       if (voController != null) {
-        for (const asNotificationName of Reflect.ownKeys(voController._commandMap)) {
-          voController.removeCommand(asNotificationName);
+        // for (const asNotificationName of Reflect.ownKeys(voController._commandMap)) {
+        //   voController.removeCommand(asNotificationName);
+        // }
+        for (const asNotificationName of container._bindingDictionary._map) {
+          voController.removeCommand(asNotificationName[0]);
         }
         Controller._instanceMap[asKey] = undefined;
         delete Controller._instanceMap[asKey];
@@ -90,11 +95,14 @@ export default (Module) => {
       }
       let vCommand;
       const vsName = aoNotification.getName();
-      vCommand = this._commandMap[vsName];
+      // vCommand = this._commandMap[vsName];
+      vCommand = container.get(vsName);
       if (vCommand == null) {
         const vsClassName = this._classNames[vsName];
         if (!_.isEmpty(vsClassName)) {
-          vCommand = this._commandMap[vsName] = this.ApplicationModule.NS[vsClassName];
+          // vCommand = this._commandMap[vsName] = this.ApplicationModule.NS[vsClassName];
+          container.bind(vsName).to(this.ApplicationModule.NS[vsClassName]);
+          vCommand = container.get(vsName);
         }
       }
       if (vCommand != null) {
@@ -105,9 +113,13 @@ export default (Module) => {
     }
 
     @method registerCommand(asNotificationName: string, aCommand: Class<CoreObject>): void {
-      if (!this._commandMap[asNotificationName]) {
+      // if (!this._commandMap[asNotificationName]) {
+      //   this._view.registerObserver(asNotificationName, Module.NS.Observer.new(this.executeCommand, this));
+      //   this._commandMap[asNotificationName] = aCommand;
+      // }
+      if (!container.get(asNotificationName)) {
         this._view.registerObserver(asNotificationName, Module.NS.Observer.new(this.executeCommand, this));
-        this._commandMap[asNotificationName] = aCommand;
+        container.bind(asNotificationName).to(aCommand);
       }
     }
 
@@ -119,22 +131,27 @@ export default (Module) => {
       if (asClassName == null) {
         asClassName = asNotificationName;
       }
-      if (!this._commandMap[asNotificationName]) {
+      // if (!this._commandMap[asNotificationName]) {
+      //   this._view.registerObserver(asNotificationName, Module.NS.Observer.new(this.executeCommand, this));
+      //   this._classNames[asNotificationName] = asClassName;
+      // }
+      if (!container.get(asNotificationName)) {
         this._view.registerObserver(asNotificationName, Module.NS.Observer.new(this.executeCommand, this));
         this._classNames[asNotificationName] = asClassName;
       }
     }
 
     @method hasCommand(asNotificationName: string): boolean {
-      return (this._commandMap[asNotificationName] != null) || (this._classNames[asNotificationName] != null);
+      // return (this._commandMap[asNotificationName] != null) || (this._classNames[asNotificationName] != null);
+      return (container.get(asNotificationName) != null) || (this._classNames[asNotificationName] != null);
     }
 
     @method removeCommand(asNotificationName: string): void {
       if (this.hasCommand(asNotificationName)) {
         this._view.removeObserver(asNotificationName, this);
-        this._commandMap[asNotificationName] = undefined;
+        // this._commandMap[asNotificationName] = undefined;
         this._classNames[asNotificationName] = undefined;
-        delete this._commandMap[asNotificationName];
+        // delete this._commandMap[asNotificationName];
         delete this._classNames[asNotificationName];
       }
     }
@@ -148,7 +165,7 @@ export default (Module) => {
       assert(Controller._instanceMap[asKey] == null, Controller.MULTITON_MSG);
       this._multitonKey = asKey;
       Controller._instanceMap[asKey] = this;
-      this._commandMap = {};
+      // this._commandMap = {};
       this._classNames = {};
       this._initializeController();
     }
