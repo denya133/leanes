9b55c27ab2febfa3100e4070f3f8226679815219
Irina M <iramark@leansdk.net>
2020-10-16 15:12:12 +0300

    Added tests for decorator method

---
+++ test/es/decorators/method-test.js
@@ -0,0 +1,31 @@
+const { expect, assert } = require('chai');
+const LeanES = require("../../../src/leanes/index.js").default;
+const {
+  CoreObject,
+  initialize, module:moduleD, method, nameBy, meta
+} = LeanES.NS;
+
+describe('method', () => {
+  describe('method(target, key, descriptor)', () => {
+    it('should add method in metaObject', () => {
+
+      @initialize
+      class Test extends LeanES {
+        @nameBy static  __filename = 'Test';
+        @meta static object = {};
+      }
+
+      @initialize
+      @moduleD(Test)
+      class SubTest extends CoreObject {
+        @nameBy static  __filename = 'Test';
+        @meta static object = {};
+        @method testMethod() {};
+        @method static testStaticMethod() {};
+      }
+      assert.equal(SubTest.metaObject.parent.data.instanceMethods.testMethod, SubTest.new().testMethod);
+      assert.equal(SubTest.metaObject.parent.data.classMethods.testStaticMethod, SubTest.testStaticMethod);
+    });
+  });
+});
+
